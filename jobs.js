38b948ca288670a91532e1d54ae2d012aa284543
André Larsen <andrewsondergaard@gmail.com>
2020-08-04 21:44:22 +0200

    Feature: script accounts for no current jobs

--- jobs.js
+++ jobs.js
@@ -1,12 +1,12 @@
-const ulJobContainer = document.querySelector('.jobs-search-results__list');
-const jobs = ulJobContainer.querySelectorAll('.artdeco-list__item ')
-const jobList = []
+let ulJobContainer = document.querySelector('.jobs-search-results__list');
+let jobs = ulJobContainer.querySelectorAll('.artdeco-list__item ')
+let jobList = []
 
 // Remember to scroll to the bottom of the document to avoid null values
 jobs.forEach((job, index) => {
-    const jobTitle = job.querySelector('.job-card-list__title').text.trim();
-    const jobLink = job.querySelector('.job-card-container__link').href.trim();
-    const timeOfPost = job.querySelector('time').innerText;
+    let jobTitle = job.querySelector('.job-card-list__title').text.trim();
+    let jobLink = job.querySelector('.job-card-container__link').href.trim();
+    let timeOfPost = job.querySelector('time').innerText;
 
     // Split text into array to access the day value
     let splitString = timeOfPost.split(' ')
@@ -21,6 +21,10 @@ jobs.forEach((job, index) => {
     }
 })
 
-jobList.forEach((job) => {
-    console.log(job)
-})
+if (jobList.length === 0) {
+    console.log("no jobs")
+} else {
+    jobList.forEach((job) => {
+        console.log(job)
+    })
+}
