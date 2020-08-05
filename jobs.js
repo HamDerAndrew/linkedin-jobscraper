aa070a06ab46e0eae2db24297339bf1241e96532
André Larsen <andrewsondergaard@gmail.com>
2020-08-06 00:32:20 +0200

    Chore: removed unused code

--- jobs.js
+++ jobs.js
@@ -1,36 +1,3 @@
-let ulJobContainer = document.querySelector('.jobs-search-results__list');
-let jobs = ulJobContainer.querySelectorAll('.artdeco-list__item ')
-let jobList = []
-
-// Remember to scroll to the bottom of the document to avoid null values
-jobs.forEach((job, index) => {
-    let jobTitle = job.querySelector('.job-card-list__title').text.trim();
-    let jobLink = job.querySelector('.job-card-container__link').href.trim();
-    let timeOfPost = job.querySelector('time').innerText;
-
-    // Split text into array to access the day value
-    let splitString = timeOfPost.split(' ')
-
-    if (parseInt(splitString[0]) < 2 && splitString[1] === "dag") {
-        const jobObj = {
-            jobTitle: jobTitle,
-            jobLink: jobLink
-        }
-    
-        jobList.push(jobObj)
-    }
-})
-
-if (jobList.length === 0) {
-    console.log("no jobs")
-} else {
-    jobList.forEach((job) => {
-        console.log(job)
-    })
-}
-
-
-// V2
 let jobsUl = document.querySelector('.card-list, card-list--tile, jobs-jymbii__list');
 // Remember to remove the first item in the list on the site as it is not a "job post", but some profile premium offer
 let jobsNode = jobsUl.querySelectorAll('.jobs-jymbii__list-item, .jobs-jymbii__list-item--job-card-addon, .card-list__item, .a11y-job-card');
