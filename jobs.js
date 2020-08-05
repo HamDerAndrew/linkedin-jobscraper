781ff80ad5d547bdca7e2674e312457dc1edf53e
André Larsen <andrewsondergaard@gmail.com>
2020-08-06 00:31:44 +0200

    Chore: fixed duplicate creation of job object

--- jobs.js
+++ jobs.js
@@ -33,7 +33,7 @@ if (jobList.length === 0) {
 // V2
 let jobsUl = document.querySelector('.card-list, card-list--tile, jobs-jymbii__list');
 // Remember to remove the first item in the list on the site as it is not a "job post", but some profile premium offer
-let jobsNode = jobsUl.querySelectorAll('.artdeco-card, .jobs-jymbii__list-item, .jobs-jymbii__list-item--job-card-addon, .card-list__item, .a11y-job-card ');
+let jobsNode = jobsUl.querySelectorAll('.jobs-jymbii__list-item, .jobs-jymbii__list-item--job-card-addon, .card-list__item, .a11y-job-card');
 let jobList = Array.from(jobsNode);
 let count = 0;
 let earlybirdJobs = [];
@@ -46,22 +46,33 @@ jobList.forEach((job, index) => {
 
     // Split text into array to access the day value
     let splitTimeStamp = timeStamp.split(' ');
-    let amountOfTime = parseInt(splitTimeStamp[0])
-    let dayOrHour = splitTimeStamp[1]
-    
-    if ( ((amountOfTime < 4 && dayOrHour === "dage") || (amountOfTime === 1 && dayOrHour === "dag")) || (amountOfTime < 24 && dayOrHour === "timer")  ) {
+    let amountOfTime = parseInt(splitTimeStamp[0]);
+    let typeOfDate = splitTimeStamp[1];
+
+    if (amountOfTime < 5 && typeOfDate === "dage") {
         const jobObj = {
             name: jobTitleContainer,
             link: jobLink,
             posted: timeStamp
         }
-        
+        earlybirdJobs.push(jobObj)
+    } else if (amountOfTime === 1 && typeOfDate === "dag") {
+        const jobObj = {
+            name: jobTitleContainer,
+            link: jobLink,
+            posted: timeStamp
+        }
+        earlybirdJobs.push(jobObj)
+    } else if (amountOfTime < 24 && typeOfDate === "timer") {
+        const jobObj = {
+            name: jobTitleContainer,
+            link: jobLink,
+            posted: timeStamp
+        }
         earlybirdJobs.push(jobObj)
     } else {
-        console.log("no jobs according to the criteria")
+        console.log("Not compliant with criteria")
     }
-
 })
 
-console.log(count)
 console.log(earlybirdJobs)
