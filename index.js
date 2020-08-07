cc98f23e664f8d7e7f92fdda7b77427ed3a78c64
André Larsen <andrewsondergaard@gmail.com>
2020-08-07 14:06:34 +0200

    Feature: navigating to jobpage and scrolling down

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require('dotenv').config({path: './config/env.env' });
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://dk.linkedin.com/');
+  await page.setViewport({
+    width: 1200,
+    height: 800
+});
 
+  // Login
   const loginEmail = await page.$('#session_key');
   const loginPass = await page.$('#session_password');
   const loginButton = await page.$('.sign-in-form__submit-button')
@@ -14,8 +19,37 @@ require('dotenv').config({path: './config/env.env' });
   await loginPass.click();
   await page.keyboard.type(process.env.PASSWORD);
   await loginButton.click();
+  await page.waitForNavigation({waitUntil: 'domcontentloaded'})
 
-  await page.screenshot({path: 'example.png'});
+  // Go to Job page
+  const jobListItem = await page.$('#jobs-nav-item');
+  const jobLink = await jobListItem.$('.nav-item__link nav-item__link--underline, .js-nav-item-link');
+  await jobLink.click();
+  await page.waitForNavigation({waitUntil: 'domcontentloaded'})
 
-  await browser.close();
+  // Scroll down to the bottom of the JobPage to load in more jobs
+  await autoScroll(page)
+
+  // await browser.close();
 })();
+
+const autoScroll = async (page) => {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      let distance = 140;
+      const scrollTimer  = setInterval(() => {
+        // Get the scrollable height from the DOM body
+        const scrollHeight = document.body.scrollHeight;
+        // Scroll down by x amount of distance
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(scrollTimer)
+          resolve()
+        }
+      }, 500)
+    })
+  })
+}
